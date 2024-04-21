4bfc487f5dce062e0ebe44da4bea5173a94ad3bf
Nikhil Pujar <nikhilpujar23@gmail.com>
2024-04-21 17:17:13 +0530

    fixed bugs in main function

--- nameless/__main__.py
+++ nameless/__main__.py
@@ -1,75 +1,14 @@
-# #!/usr/bin/python
-# # -*- coding: UTF-8 -*-
-
-# """
-# main.py
-
-# @author ejnp
-# """
-
-# from nameless.lexer import Lexer
-# from nameless.parser import Parser, ParserError
-# from nameless.visitors import BetaReduction
-
-
-# def interpret(input_string, print_reductions=False):
-#     """Performs normal order reduction on the given string lambda calculus
-#     expression. Returns the expression's normal form if it exists.
-#     """
-#     lexer = Lexer(input_string)
-#     try:
-#         ast = Parser(lexer).parse()
-#     except ParserError as discrepancy:
-#         print( 'ParseError: ' + discrepancy.message)
-#         return None
-#     normal_form = False
-#     while not normal_form:
-#         reducer = BetaReduction()
-#         reduced_ast = reducer.visit(ast)
-#         normal_form = not reducer.reduced
-#         if print_reductions:
-#             print( unicode(ast))
-#         ast = reduced_ast
-#     return unicode(ast)
-
-
-# def main():
-#     """Begins an interactive lambda calculus interpreter"""
-#     print( "nameless!\nType 'quit' to exit.")
-#     while True:
-#         read = input('> ')
-#         if read == 'quit':
-#             break
-#         if read != '':
-#             interpret(read, print_reductions=True)
-
-
-# if __name__ == '__main__':
-#     main()
-
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-
-"""
-main.py
-
-@author ejnp
-"""
-
-from nameless.lexer import Lexer
-from nameless.calculus_parser import Parser, ParserError
-from nameless.visitors import BetaReduction
-
+from lexer import Lexer
+from calculus_parser import Parser, ParserError
+from visitors import BetaReduction
 
 def interpret(input_string, print_reductions=False):
-    """Performs normal order reduction on the given string lambda calculus
-    expression. Returns the expression's normal form if it exists.
-    """
+    """Performs normal order reduction on the given string lambda calculus expression."""
     lexer = Lexer(input_string)
     try:
         ast = Parser(lexer).parse()
     except ParserError as discrepancy:
-        print('ParseError: ' + discrepancy.message)
+        print('ParseError:', discrepancy)
         return None
     normal_form = False
     while not normal_form:
@@ -77,21 +16,23 @@ def interpret(input_string, print_reductions=False):
         reduced_ast = reducer.visit(ast)
         normal_form = not reducer.reduced
         if print_reductions:
-            print(ast)
+            print('Reduction step:', str(ast))
         ast = reduced_ast
     return str(ast)
 
-
 def main():
-    """Begins an interactive lambda calculus interpreter"""
-    print("nameless!\nType 'quit' to exit.")
+    """Begins an interactive lambda calculus interpreter with support for mathematical functions."""
+    print("Welcome to the Lambda Calculus Interpreter with Extensions!\n"
+          "Type 'quit' to exit.\n"
+          "You can now use 'sin', 'cos', and 'exp' functions, e.g., sin(0.5), cos(x), exp(λx.x).")
     while True:
         read = input('> ')
         if read == 'quit':
             break
-        if read != '':
-            interpret(read, print_reductions=True)
-
+        if read:
+            result = interpret(read, print_reductions=True)
+            if result is not None:
+                print('Result:', result)
 
 if __name__ == '__main__':
     main()
